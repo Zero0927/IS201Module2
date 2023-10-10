825c419fd427e52f68d836f6d0113e081b78e223
Scott Zhou <zhouscott@hotmail.com>
2023-10-10 15:14:23 -0700

    Module 2 In class exercise

---
+++ IS201Module2.py
@@ -0,0 +1,62 @@
+
+
+# message = "Hello World"
+# print(message)
+
+# name = "ada lovelace"
+# print(name.title())
+# print(name.upper())
+# print(name.lower())
+
+# first_name = "Scott"
+# last_name = "Zhou"
+# full_name = f"{first_name}"+" "+ f"{last_name}"
+# print(full_name)
+
+# cars = ['audi', 'bmw', 'subaru', 'toyota']
+# for car in cars:
+#     if car == 'bmw':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+# requested_topping = 'mushrooms'
+# if requested_topping != 'anchovies':
+#     print("Hold the achovies!")
+# answer = 17
+# if answer != 42:
+#     print("That is not the correct number.")
+# if answer > 42:
+#     print("This is greater than 42")
+# if answer < 42:
+#     print("This is less than 42")          
+
+# age_0 = 22
+# age_1 = 18
+# # if age_0 >= 21 and age_1 >= 21:
+# #     print("They can drink")
+    
+# if age_0 <= 21 or age_1 <= 21:
+#     print("They can't drink")
+    
+# requested_toppings = ['mushrooms','onions','pineapple']
+# if 'mushrooms' in requested_toppings:
+#     print("We found mushrooms")
+# if 'pepperoni' in requested_toppings:
+#     print("We found pepperoni")
+    
+# banned_users = ['andrew','carolina','david']
+# user = 'marie'
+# if user not in banned_users:
+#     print(f"{user.title()}, you can post a response if you wish")
+
+# message = input("Tell me something, and I will repeat it back to you:")
+# print(message)
+
+# name = input("Please enter your name:")
+# print(f"\nHello, {name}!")
+
+current_number = 5
+while current_number >=1 :
+    print(current_number)
+    current_number -= 2
+#   current_number = current_number  - 1
